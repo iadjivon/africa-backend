3ac607ccec5aedf5ae6744929511ef62237add43
Ida Adjivon <idaadjivon@idas-mbp-2.myfiosgateway.com>
2020-12-24 03:52:46 -0500

    restructuring the model to better display the recipes

--- models/recipes.js
+++ models/recipes.js
@@ -1,20 +1,40 @@
 const mongoose = require('mongoose');
 
-// const instructionsList = new mongoose.Schema ({ name: String}) 
 
-// const ingredientList = new mongoose.Schema ({ any: [{}]})
+// const recipeSchema = new mongoose.Schema({
+//     recipeName:  { type: String },
+//     cookTime:  { type: Number  },
+//     description:  { type: String },
+//     servingSize:  { type: Number },
+//     ingredients:  {type: String },
+//     instructions:  {type: String },
+//     recipeCompleted: Boolean
+// });
 
-const recipeSchema = new mongoose.Schema({
-    name:  { type: String, required: true },
-    cookTime:  { type: Number  },
-    description:  { type: String },
-    servingSize:  { type: Number },
-    ingredients:  {type: String },
-    instructions:  {type: String },
-    recipeCompleted: Boolean
+const countryRecipeSchema = new mongoose.Schema({
+    countryName: {type: String, required: true},
+        recipeInfo: [{
+            recipeName:  { type: String },
+            cookTime:  { type: Number  },
+            description:  { type: String },
+            servingSize:  { type: Number },
+            ingredients:  {type: String },
+            instructions:  {type: String },
+            recipeCompleted: Boolean
+        }
+
+        ]
+
+    // name:  { type: String, required: true },
+    // cookTime:  { type: Number  },
+    // description:  { type: String },
+    // servingSize:  { type: Number },
+    // ingredients:  {type: String },
+    // instructions:  {type: String },
+    // recipeCompleted: Boolean
 });
 
-const  Recipe = mongoose.model('Recipe', recipeSchema);
+const  recipeByCountry = mongoose.model('Recipe', countryRecipeSchema);
 
-module.exports = Recipe;
+module.exports = recipeByCountry;
 
